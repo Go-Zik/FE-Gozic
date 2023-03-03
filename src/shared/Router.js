369dc90f96ab13448890910c443113a1455e5a72
jaeuk Lee <lskkhj1005@naver.com>
2023-03-04 00:05:40 +0900

    chore: fix app.js, router

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,15 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from '../pages/Login'
 import Detail from '../pages/Detail'
 import Mainpage from '../pages/Mainpage'
-import Signup from '../pages/signup'
+import Signup from '../pages/Signup'
 
 const Router = () => {
 
     return (
     <BrowserRouter>
       <Routes>
-          {/* <Route path="/" element={<MainPage />} /> */}
-          <Route path="/mainpage" element={<Mainpage />} />
+          <Route path="/" element={<Mainpage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/detail" element={<Detail />} />
